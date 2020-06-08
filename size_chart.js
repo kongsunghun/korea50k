efae6f190119d9281bbfdc992a37841470f28f19
kongsunghun <kongom2@gmail.com>
2020-06-08 16:40:27 +0900

    menu60

--- size_chart.js
+++ size_chart.js
@@ -5,10 +5,10 @@ function size() {
 window.onclick = function (event) {
     if (!event.target.matches('.size_chartbtn')) {
 
-        var chart = document.getElementsByClassName("size_chart");
+        var charts = document.getElementsByClassName("size_chart");
         var c;
-        for (c = 0; c < chart.length; c++) {
-            var openchart = chart[c];
+        for (c = 0; c < charts.length; c++) {
+            var openchart = charts[c];
             if (openchart.classList.contains('chart_show')) {
                 openchart.classList.remove('chart_show');
             }
