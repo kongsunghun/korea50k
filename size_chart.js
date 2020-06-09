83be2a85f1fa5f2332b14aac4bf06d3245ba1911
kongsunghun <kongom2@gmail.com>
2020-06-09 18:55:40 +0900

    course_info

--- size_chart.js
+++ size_chart.js
@@ -5,12 +5,12 @@ function size() {
 window.onclick = function (event) {
     if (!event.target.matches('.size_chartbtn')) {
 
-        var charts = document.getElementsByClassName("size_chart");
-        var c;
-        for (c = 0; c < charts.length; c++) {
-            var openchart = charts[c];
-            if (openchart.classList.contains('chart_show')) {
-                openchart.classList.remove('chart_show');
+        var dropdowns = document.getElementsByClassName("size_chart");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('chart_show')) {
+                openDropdown.classList.remove('chart_show');
             }
         }
     }
